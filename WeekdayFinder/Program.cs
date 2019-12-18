8f5ef12045a822c9c41f1f6c409b107f0c9f30a7
Anthony Golovingit config --global user.name Anthony <AGOLOVIN97@live.com>
2019-12-17 22:23:53 -0800

    Only added initial files and possible references

---
+++ WeekdayFinder/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace WeekdayUI
+{
+    public class Program
+    {
+        // https://www.youtube.com/watch?v=TKSpLH9CK_c
+        // https://www.c-sharpcorner.com/UploadFile/mahesh/dictionary-in-C-Sharp/
+        static void Main()
+        {
+            Console.WriteLine("");
+            string p1 = Console.ReadLine().ToLower();
+
+            
+            
+                
+        }
+    }
+}
